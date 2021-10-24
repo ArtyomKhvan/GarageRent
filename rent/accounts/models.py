40e2511f8b43ac6bdfb88f419c055f6cec81016b
Artyom <hvanartem@gmail.com>
2021-10-25 02:56:06 +0600

    Создал профиль для пользователя

--- rent/accounts/models.py
+++ rent/accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(get_user_model(), related_name='profile', on_delete=models.CASCADE,
+                                verbose_name='Пользователь')
+    about_me = models.TextField(max_length=3000, null=True, blank=True, verbose_name="описание")
+    avatar = models.ImageField(null=True, blank=True, upload_to='user_pics', verbose_name='Аватар')
+    birth = models.DateField(null=True, blank=True)
