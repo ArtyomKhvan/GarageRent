809a78c647c59a2fa710679684bea20867677609
Artyom <hvanartem@gmail.com>
2021-10-25 16:40:27 +0600

    Зарегистрировал модели в админке

--- rent/accounts/models.py
+++ rent/accounts/models.py
@@ -5,6 +5,6 @@ from django.contrib.auth import get_user_model
 class Profile(models.Model):
     user = models.OneToOneField(get_user_model(), related_name='profile', on_delete=models.CASCADE,
                                 verbose_name='Пользователь')
-    about_me = models.TextField(max_length=3000, null=True, blank=True, verbose_name="описание")
+    about_me = models.TextField(max_length=3000, null=True, blank=True, verbose_name="Обо мне")
     avatar = models.ImageField(null=True, blank=True, upload_to='user_pics', verbose_name='Аватар')
     birth = models.DateField(null=True, blank=True)
--- rent/webapp/admin.py
+++ rent/webapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from rent.webapp.models import Car, Photo, Platform, Review
 
-
+admin.site.register(Car)
+admin.site.register(Photo)
+admin.site.register(Platform)
+admin.site.register(Review)
