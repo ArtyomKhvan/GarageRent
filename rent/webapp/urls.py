a309054cd910f5a155773c594ba3361910a2e4f8
Artyom <hvanartem@gmail.com>
2021-11-01 10:19:22 +0600

    Создал страницу для детального просмотра

--- rent/webapp/urls.py
+++ rent/webapp/urls.py
@@ -6,5 +6,6 @@ app_name = "webapp"
 urlpatterns = [
     path('', general_view.IndexView.as_view(), name="index"),
     path('about_us/', general_view.AboutUsView.as_view(), name="about_us"),
-    path('cars/', general_view.CarListView.as_view(), name="car_list")
+    path('cars/', general_view.CarListView.as_view(), name="car_list"),
+    path('cars/<int:pk>/', general_view.CarDetailView.as_view(), name="car_detail")
 ]
