63e4ad096674f510bc0eedad6fdede9a6545e808
Artyom <hvanartem@gmail.com>
2021-11-01 09:33:40 +0600

    Сделал стили для страницы просмотра машин

--- rent/webapp/views/general_view.py
+++ rent/webapp/views/general_view.py
@@ -19,3 +19,9 @@ class CarListView(ListView):
     ordering = ["year"]
     context_object_name = "cars"
 
+
+class CarDetailView(DetailView):
+    model = Car
+    template_name = "cars/detail.html"
+    context_object_name = "car"
+    pk_url_kwarg = "pk"
