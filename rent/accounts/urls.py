118cb814736ae2f59c2a8a2a3b0c7b6126991e54
Artyom <hvanartem@gmail.com>
2021-11-03 15:20:46 +0600

    вывел в профиле все заказы

--- rent/accounts/urls.py
+++ rent/accounts/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path("logout/", accounts_views.LogoutView.as_view(), name="logout"),
     path("create/", accounts_views.RegisterView.as_view(), name="register"),
     path("<int:pk>/", accounts_views.UserDetailView.as_view(), name="profile"),
+    path("profile/<int:pk>/edit", accounts_views.UserChangeView.as_view(), name="edit_profile"),
+    path("profile/<int:pk>/change_password", accounts_views.ChangePasswordView.as_view(), name="change_password")
 ]
--- rent/webapp/admin.py
+++ rent/webapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from webapp.models import Car, Photo, Platform, Review
+from webapp.models import Car, Photo, Platform, Review, Client
 
 admin.site.register(Car)
 admin.site.register(Photo)
 admin.site.register(Platform)
 admin.site.register(Review)
+admin.site.register(Client)
