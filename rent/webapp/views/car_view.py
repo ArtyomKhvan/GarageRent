4c48e8ce82eea7c59c81b7ce1d89d932c41fcd9e
Artyom <hvanartem@gmail.com>
2021-11-03 12:02:55 +0600

    Создал форму для создания отзыва

--- rent/webapp/views/car_view.py
+++ rent/webapp/views/car_view.py
@@ -1,6 +1,7 @@
-from django.views.generic import View, DetailView, CreateView, UpdateView, DeleteView, ListView
-from webapp.models import Car, Client
+from django.shortcuts import get_object_or_404
 from django.urls import reverse
+from django.views.generic import DetailView, CreateView, DeleteView, ListView
+from webapp.models import Car, Review
 
 
 class CarListView(ListView):
@@ -17,5 +18,20 @@ class CarDetailView(DetailView):
     pk_url_kwarg = "pk"
 
 
+class ReviewCreateView(CreateView):
+    model = Review
+    template_name = 'partial/review_form.html'
+    fields = ['text']
 
+    def form_valid(self, form):
+        form.instance.car = self.get_object()
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_object(self, queryset=None):
+        pk = self.kwargs.get("pk")
+        return get_object_or_404(Car, pk=pk)
+
+    def get_success_url(self):
+        return reverse('webapp:car_detail', kwargs={"pk": self.object.car.pk})
 
